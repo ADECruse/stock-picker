f926663238689c46a5bbd075d63708dde43d5898
Alan Cruse <alancruse86@gmail.com>
2016-12-11 14:31:31 +0100

    Final version

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,20 +1,16 @@
 # > stock_picker([17,3,6,9,15,8,6,1,10])
 # => [1,4]  # for a profit of $15 - $3 == $12
 
-def stock_picker()
-  stock_price.arr.index {|x| x == arr.sort.last}
+def stock_picker(arr)
+  best_days = []
   arr.each do |i|
-   arr.each do |x|
-     if arr.index(x) > i
-       puts x - i
-     end 
+    arr.each do |x|
+      if arr.index(x) > arr.index(i)
+        if x - i > 0
+          best_days.insert(x - i, "Buying day #{arr.index(i)} and selling day #{arr.index(x)} gives the highest possible profit of #{x - i}")
+        end
+      end
+    end
   end
+  best_days.last
 end
-
-for each number in the array minus with every other number in the array
-
-
-   # find the difference, start from the last number?
-arr.combination(2) {|a, b| puts a - b }
-
-arr.index(i)
